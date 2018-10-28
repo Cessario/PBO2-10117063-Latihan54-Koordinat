e34ca8724a625c4178f26ec64596576a5d7e9cd2
Cessario <43488314+Cessario@users.noreply.github.com>
2018-10-28 20:54:28 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117063/latihan54/koordinat/PBO210117063Latihan54Koordinat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan54.koordinat;
+
+/**
+ *
+ * @author Deadpool
+ */
+public class PBO210117063Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WarnaKoordinat ko = new WarnaKoordinat("Jingga", 10 , 4);
+    }
+    
+}
---
+++ src/pbo2/pkg10117063/latihan54/koordinat/WarnaKoordinat.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan54.koordinat;
+
+/**
+ *
+ * @author Deadpool
+ */
+public class WarnaKoordinat extends Koordinat{
+    public String namaWarna;
+
+    public WarnaKoordinat(String namaWarna, int x, int y) {
+        super(x, y);
+        this.namaWarna = namaWarna;
+        
+        System.out.println("Warna Koordinat : "+namaWarna);
+        System.out.println("Koordinat Sumbu x : " +x+ ", Y : " +y);
+    }
+
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+
+    
+    
+    
+}
